6c245d87e936b9587112982ceaa7fe9e54cb81d5
mouad <mouad.deraoui@gmail.com>
2020-12-17 21:33:07 +0100

    Add delete featue

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -95,4 +95,16 @@ class PinsController extends AbstractController
             'pin'   => $pin
         ]);
     }
+
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}/delete", name="app_pins_delete", methods="DELETE")
+     */
+    public function delete(Pin $pin, EntityManagerInterface $em): Response
+    {
+        $em->remove($pin);
+        $em->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
 }
