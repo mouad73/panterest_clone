898ef8e3cb571319b3ea86fca7386144faf437b8
mouad <mouad.deraoui@gmail.com>
2021-01-05 21:47:27 +0100

    Add flash messages and mov base layout to layouts folder

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -47,6 +47,8 @@ class PinsController extends AbstractController
             $em->persist($pin);
             $em->flush();
 
+            $this->addFlash('success', 'Pin successfully created!');
+
             return $this->redirectToRoute('app_home');
         }
 
@@ -87,6 +89,8 @@ class PinsController extends AbstractController
         if($form->isSubmitted() && $form->isValid()){
             $em->flush();
 
+            $this->addFlash('success', 'Pin successfully updated!');
+
             return $this->redirectToRoute('app_home');
         }
 
@@ -107,6 +111,7 @@ class PinsController extends AbstractController
         if($this->isCsrfTokenValid('pins_deletion_' . $pin->getId(), $request->request->get('csrf_token'))){
             $em->remove($pin);
             $em->flush();
+            $this->addFlash('info', 'Pin successfully deleted!');
         }
         
 
