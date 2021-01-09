10cfe75de97e61b6bdfbc3875dc68baa6960f75a
mouad <mouad.deraoui@gmail.com>
2021-01-10 00:42:56 +0100

    Fix issue wit ile name not showing after image selection

--- assets/app.js
+++ assets/app.js
@@ -13,3 +13,9 @@ import 'bootstrap';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+$('.custom-file-input').on('change', function(e){
+    var inputFile = e.currentTarget;
+    $(inputFile).parent().find('.custom-file-label').html(inputFile.files[0].name);
+});
