b6697edac355ce634eb64107fcc133c7cb55218b
DERAOUI Mouad <mouad.deraoui@gmail.com>
2021-04-16 17:58:30 +0000

    change welcome message when login

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -95,7 +95,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
     {
-        $request->getSession()->getFlashBag()->add('success', 'Logged in succesfully!');
+        $request->getSession()->getFlashBag()->add('success', 'Welcome '. $token->getUser()->getFullName());
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
