35dd4837b993149a33a6d7fdb655f8f6685716fe
DERAOUI Mouad <mouad.deraoui@gmail.com>
2021-04-28 10:28:07 +0000

    Explicit is better than implicite

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Pin;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -24,8 +26,8 @@ class PinType extends AbstractType
             'imagine_pattern' => 'squared_thumbnail_small',
             // 'asset_helper' => true, 
         ])
-            ->add('title')
-            ->add('description')
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
         ;
     }
 
