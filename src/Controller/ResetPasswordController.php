1d8adddbcf1419d7459023522bb2afad662089b4
DERAOUI Mouad <mouad.deraoui@gmail.com>
2021-08-16 21:54:11 +0100

    Add flash message when reset password success

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -135,6 +135,8 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
+            $this->addFlash('success','You are succefully reset your password');
+
             return $this->redirectToRoute('app_home');
         }
 
